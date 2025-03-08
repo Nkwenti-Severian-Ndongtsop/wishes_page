839d1dddab57feeca6b04ad310c69c5ade9a4410
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-08 11:08:42 +0100

    added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wishes_page/',
   optimizeDeps: {
     exclude: ['lucide-react'],
-    base: "/women_day/",
   },
 });
